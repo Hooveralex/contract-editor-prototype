73b107d35564ac4d15a5c3aa8f55700ce45056d9
Alex Hoover <Hoover.alex@gmail.com>
2017-12-21 15:37:05 -0500

    Create app.js

---
+++ clause-content/assets/js/app.js
@@ -0,0 +1,16 @@
+$('.content-nav-list-item > a').click(function(e){
+	var id = $(this).attr('href');
+	e.preventDefault();
+	$('.content-nav-list-item').removeClass('active');
+	$(this).parent('.content-nav-list-item').addClass('active');
+
+	$('.org-landing-page-content').addClass('hidden');
+	$(id).removeClass('hidden');
+
+	console.log(id);
+
+});
+
+$('.clause-panel').click(function(e){
+	$(this).find('.clause-more-info-container').toggleClass('hidden');
+});
